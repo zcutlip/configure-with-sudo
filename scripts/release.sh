0c555f03c9b6901c8e828929cf881066daf06834
Zachary Cutlip <uid000@gmail.com>
2019-11-11 14:38:18 -0800

    must call quit with a numeric exit status

--- scripts/release.sh
+++ scripts/release.sh
@@ -23,17 +23,17 @@ if ! version_is_tagged "$version";
 then
     echo "Current version $version isn't tagged."
     echo "Attempting to tag..."
-    "$DIRNAME"/tag.sh || quit "Failed to tag a release."
+    "$DIRNAME"/tag.sh || quit "Failed to tag a release." 1
 fi
 
 generate_dist(){
-    python3 setup.py sdist bdist_wheel || quit "Failed to generate source & binary distributions."
+    python3 setup.py sdist bdist_wheel || quit "Failed to generate source & binary distributions." 1
 }
 
 version=$(current_version);
 
 echo "About to post the following distribution files to pypi.org."
-ls -1 dist/"$PROJECT_NAME"-$version.*
+ls - dist/"$PROJECT_NAME"-$version.*
 
 if prompt_yes_no;
 then
