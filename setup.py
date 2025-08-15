5d87f6d70c0a474c302fe97f815dec24c75d3e42
Zachary Cutlip <uid000@gmail.com>
2025-08-15 12:35:28 -0700

    migrate to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,24 +1,7 @@
 from setuptools import setup
 
-about = {}
-with open("configure_with_sudo/__about__.py") as fp:
-    exec(fp.read(), about)
+# keep this around so the following work:
+# python3 ./setup.py --version
+# python3 ./setup.py --name
 
-with open("README.md", "r") as fp:
-    long_description = fp.read()
-
-setup(
-    name=about["__title__"],
-    version=about["__version__"],
-    description=about["__summary__"],
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Zachary Cutlip",
-    author_email="uid000@gmail.com",
-    url="https://github.com/zcutlip/configure-with-sudo",
-    license="MIT",
-    packages=["configure_with_sudo"],
-    python_requires=">=2.7",
-    install_requires=[],
-    package_data={"configure_with_sudo": ["config/*"]},
-)
+setup()
